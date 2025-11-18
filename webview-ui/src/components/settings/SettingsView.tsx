9d01b70739a07a1f78bc7f4c839751504ed04ae3
Hannes Rudolph <hrudolph@gmail.com>
2025-11-18 07:30:55 -0700

    fix

--- webview-ui/src/components/settings/SettingsView.tsx
+++ webview-ui/src/components/settings/SettingsView.tsx
@@ -837,7 +837,7 @@ const SettingsView = forwardRef<SettingsViewRef, SettingsViewProps>(({ onDone, t
 						<About
 							telemetrySetting={telemetrySetting}
 							setTelemetrySetting={setTelemetrySetting}
-							taskHistoryRetention={String(taskHistoryRetention ?? "never")}
+							taskHistoryRetention={taskHistoryRetention ?? "never"}
 							setTaskHistoryRetention={(value) =>
 								setCachedStateField(
 									"taskHistoryRetention",
